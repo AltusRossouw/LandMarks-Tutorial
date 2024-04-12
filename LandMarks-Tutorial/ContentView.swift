59549301dba26623a7318f8a2f8006e5bd226ac1
Altus <altusrossouw2@gmail.com>
2024-04-12 14:23:00 +0200

    Initial Commit

---
+++ LandMarks-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks-Tutorial
+//
+//  Created by Altus Rossouw on 12.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandMarks-Tutorial/LandMarks_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarks_TutorialApp.swift
+//  LandMarks-Tutorial
+//
+//  Created by Altus Rossouw on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandMarks_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
