17486e79a95e95814478a3ef0ae740943e7bb292
Altus <altusrossouw2@gmail.com>
2024-04-12 15:30:51 +0200

    Creating image and coordinate variables

--- LandMarks-Tutorial/Landmark.swift
+++ LandMarks-Tutorial/Landmark.swift
@@ -1,8 +1,28 @@
-//
-//  Landmark.swift
-//  LandMarks-Tutorial
-//
-//  Created by Altus Rossouw on 12.04.2024.
-//
-
 import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude : coordinates.latitude,
+            longitude : coordinates.longitude)
+    }
+    
+    struct Coordinates : Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
