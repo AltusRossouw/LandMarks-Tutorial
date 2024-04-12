d9750add33d6771a97ff5503d7713c2090e34105
Altus <altusrossouw2@gmail.com>
2024-04-12 14:47:32 +0200

    Create a custom image view

---
+++ LandMarks-Tutorial/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  LandMarks-Tutorial
+//
+//  Created by Altus Rossouw on 12.04.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
